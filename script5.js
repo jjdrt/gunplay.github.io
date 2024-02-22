a49435490881a9e2c2e6964bb77bd77910f5a43d
jjdrt <55495547+jjdrt@users.noreply.github.com>
2024-02-22 21:59:14 +0800

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,12 @@
+//This is the loading screen to load up the website's assets
+
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+ 
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        setTimeout(()=>{
+            document.querySelector("#loader").style.display = "none";
+    }, 4000)
+    }
+};
